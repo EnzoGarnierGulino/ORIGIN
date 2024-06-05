02ade5e916c92396d02158c608f9371e68360d65
BoijyProgrammerX <166648999+djboijy@users.noreply.github.com>
2024-06-05 20:37:47 +0300

    Made everything working in router.js!☀

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import About from '@/components/About.vue';
 import SongList from "@/components/SongList.vue";
 import Artists from "@/components/Artists.vue";
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -8,6 +10,7 @@ const router = createRouter({
         { path: '/', name: 'home', component: SongList },
         { path: '/songs', name: 'songs', component: SongList },
         { path: '/artists', name: 'artists', component: Artists },
+        { path: '/about', name: 'about', component: About },
     ]
 });
 
