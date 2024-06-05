eeb378af1b0cd539793176f60ca34867e940ff2c
BoijyProgrammerX <166648999+djboijy@users.noreply.github.com>
2024-06-05 20:33:34 +0300

    Added About Page!🔥

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,17 +1,175 @@
 <template>
-  <div>
-    <h1>about here</h1>
-  </div>
+    <div class="aboutpage">
+        <header class="header">
+            <h1>We are ORIGIN. A Label that provides the music you love.</h1>
+        </header>
+    </div>
+    <br />
+    <br />
+    <div>
+        <h1 class="classicheader">--About Us--</h1>
+    </div>
+    <p class="paragraphtext">
+        ORIGIN Records is a music label founded by Origin and Tuni, also managed by Minix and the staff team. On our label you can listen to nostalgic, viby and gaming music of any sorts. We provide nostalgic circle visualizers with the music, making it more fun. We are inspired by NCS(NoCopyrightSounds) label and we are trying to revive it's old style! The magic of the music we upload.. We try to gain back old goosebumps when music starts playing ✨! The label also promotes new artists, and you have a chance to get your song distributed and posted on the label's social media. More about it will be noticed below!'
+    </p>
+    <br />
+    <h1 class="classicheader">--Socials--</h1>
+    <p class="paragraphtext">
+        ORIGIN Records can review your demo music and release it after in their Discord Server. We also have lots of social media where we actually do it! Click the buttons below to open all of our socials!💾
+    </p>
+
+    <a :href="'https://www.instagram.com/origin.records/'" target="_blank">
+        <img class="socialmediabutton" src="https://th.bing.com/th/id/R.24223c92569a035c382c2b658e9789a0?rik=SsJgrmXtdiNaEw&pid=ImgRaw&r=0"
+             alt="ORIGIN Records Instagram!" />
+    </a>
+    <a :href="'https://www.youtube.com/@origin_records'" target="_blank">
+        <img class="socialmediabutton" src="https://1000logos.net/wp-content/uploads/2017/05/New-YouTube-logo.jpg"
+             alt="ORIGIN Records Youtube!" />
+    </a>
+    <a :href="'https://open.spotify.com/playlist/6gxr6s1C9YImDsoJgiVGxC'" target="_blank">
+        <img class="socialmediabutton" src="https://www.freepnglogos.com/uploads/spotify-logo-png/image-gallery-spotify-logo-21.png"
+             alt="ORIGIN Records Spotify Playlist!" />
+    </a>
+    <a :href="'soundcloud.com/origin_records'" target="_blank">
+        <img class="socialmediabutton" src="../assets/SoundCloud.jpg"
+             alt="ORIGIN Records SoundCloud!" />
+    </a>
+    <a :href="'discord.gg/uVUKUnq3vm'" target="_blank">
+        <img class="socialmediabutton" src="https://th.bing.com/th/id/OIP.EZkdm25nbQDs_PjFN8hmewAAAA?rs=1&pid=ImgDetMain"
+             alt="ORIGIN Records Official Discord Server!" />
+    </a>
+    <br />
+    <br />
+    <br />
+    <h1 class="classicheader">--Frequently Asked Questions--</h1>
+
+    <br />
+    <br />
+    <br />
+    <h1 style="font-family: Arial; text-shadow: inherit 5px; font-weight: bold; text-align: left">Is the music on the label NO COPYRIGHT?</h1>
+    <br />
+    <p class="paragraphtext">
+        Not really. Some songs are, while the others not. Please ask the song owner before using it for any piece of your work! We are ORIGIN Records, no limits, just music🎵! <br /> <br />
+    </p>
+    <br />
+    <br />
+    <br />
+    <h1 style="font-family: Arial; text-shadow: inherit 5px; font-weight: bold; text-align: left">How can I sumbit my song to the label?</h1>
+    <br />
+    <p class="paragraphtext">
+        You can do it, and it's very simple! Firstly, join our discord server! Next, create a ticket in #submission channel and follow these rules when submitting:<br /> ▹Only send unreleased (does not count for Newgrounds) , copyright free music that YOU have created.
+        If we find out that your song is already on any other platform, we will cancel your release.
+        <br />
+        ▹Send your submission in .mp3, .flac or in a google doc so we can easily distribute it.
+        <br />
+        ▹If you want to release on a specific date, way or if you have a coverart send it to us.
+        <br />
+        ▹DO NOT ask about the state of your submission. If you do it, your release gonna be cancelled aswell!
+        <br />
+        ▹Coverarts have to have the ORIGIN Logo or any text like ORIGIN in it.
+        <br />
+        ▹Only open a ticket if you want to submit a song. If you open a ticket for no reason, we will ban you from creating tickets.
+        <br />
+        ▹Vocals in your songs HAS TO BE Royality and Copyright free. We are not asking, but we want to see your license that youre allowed to sell this vocal within your music online!
+        <br />
+        ▹Please send links to all social media you'd like to have linked in the video. This will make it easier for us to distribute on your spotify, or cooperate with your Instagram etc
+        <br />
+        ▹We only want to see FINISHED songs. We wont wait another 5 demos with improvements that you send...
+        <br />
+        <br />
+        You can join our discord to submit here:
+
+        <br />
+    </p>
+    <a :href="'discord.gg/uVUKUnq3vm'" target="_blank">
+        <img class="socialmediabutton" src="https://th.bing.com/th/id/OIP.EZkdm25nbQDs_PjFN8hmewAAAA?rs=1&pid=ImgDetMain"
+             alt="ORIGIN Records Official Discord Server!" />
+    </a>
+    <br />
+    <p class="paragraphtext">HAPPY SUBMITTING!</p>
+    <h1 style="font-family: Arial; text-shadow: inherit 5px; font-weight: bold; text-align: left">Are there any limits to genres submitted to the label?</h1>
+    <br />
+    <p class="paragraphtext">
+        No limits to genres, just music! We upload ANY kind of music. Feel free to submit and have fun listening to it!
+    </p>
 </template>
 
 <script>
 
 export default {
   name: 'About'
+  
 }
 
 </script>
 
 <style scoped>
 
+
+
+    .aboutpage {
+        font-family: Arial, sans-serif, Bell MT;
+    }
+
+
+    .header {
+        border: dotted 6px;
+        scroll-padding-inline-start: initial 5px;
+        box-shadow: inset 20px;
+        transform-box: border-box;
+        box-sizing: border-box;
+        outline: dotted 3px;
+        background-color: #323232;
+        outline-color: darkblue;
+        text-shadow: inherit 10px;
+        color: white;
+        font-family: Arial;
+        padding: 20px;
+        text-align: center;
+    }
+
+    .header h1 {
+            margin: 0;
+            font-size: 2.5em;
+    }
+
+    .classicheader {
+        font-family: Arial;
+        font-size: 4.5em;
+        margin: 17px;
+        text-shadow: 2px 2px 4px rgba(0, 2, 5, 0.3);
+        text-align: center;
+    }
+
+    .paragraphtext {
+        font-family: 'Arial';
+        font-size: 2em;
+        color: #000000;
+        text-shadow: 2px 2px 4px rgba(0, 2, 5, 0.3);
+        display: inline-block;
+        margin: 17px;
+        transition: all 0.3s ease;
+        animation: ease-in 0.5s;
+    }
+
+        .paragraphtext:hover {
+            text-shadow: none;
+            -webkit-background-clip: border-box;
+        }
+
+
+    .socialmediabutton {
+        margin: 0 10px;
+        animation: ease-in;
+        cursor: pointer;
+        width: 70px;
+        height: 70px;
+    }
+
+    .img-normal {
+        width: 75px;
+        height: 75px;
+        margin: 30px;
+        border: solid 6px;
+    }
 </style>
